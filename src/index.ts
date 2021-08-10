72a821d76dbc8680d146f18c489a96aa11e939c7
Douglas Camargo <dcamargo@tmrw.org>
2021-08-10 10:31:07 -0300

    Add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import routes from './routes'
 import dotenv from 'dotenv'
 import express from 'express'
+import cors from 'cors'
 
 dotenv.config()
 const app = express()
 const PORT = process.env.SHOP_API_PORT ?? 3333
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(routes)
